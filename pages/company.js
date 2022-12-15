b877a3211f914f732090e532e4cc3d462a0f34e7
enespusa <pusaenes@gmail.com>
2022-12-15 18:47:14 +0300

    added company

--- pages/company.js
+++ pages/company.js
@@ -1,36 +1,159 @@
 import Head from "next/head";
+import React,{ useState } from "react";
 
-function Company() {
-    return (<>
-    <Head>
-    <title/>
-    <meta charSet="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css" />
-    <link rel="stylesheet" href="css/animate.css" />
-    <link rel="stylesheet" href="css/owl.carousel.min.css" />
-    <link rel="stylesheet" href="css/owl.theme.default.min.css" />
-    <link rel="stylesheet" href="css/magnific-popup.css" />
-    <link rel="stylesheet" href="css/aos.css" />
-    <link rel="stylesheet" href="css/ionicons.min.css" />
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css" />
-    <link rel="stylesheet" href="css/jquery.timepicker.css" />
-    <link rel="stylesheet" href="css/flaticon.css" />
-    <link rel="stylesheet" href="css/icomoon.css" />
-    <link rel="stylesheet" href="css/style.css" />
-    </Head>
-    <div>
-        <h1>WANT TO JOIN US WITH YOUR COMPANY?</h1>
-    </div>
+export const getStaticProps = async () => {
+  const request = await fetch(
+      `http://localhost:2000/api/customers`
+  );
+  const customers = await request.json();
+
+  return {
+      props: {
+          customers,
+      },
+  };
+}
+function signUp({ customers }){
+
+  const [Email,setEmail]=useState("");
+  
+  const customersArray = Array.from(customers.data);
+
+  const hasMatch = customersArray.some(
+      (customer) => customer.Email === Email
+  );
+
+
+  async function Register()
+  {
+      
+    if (!hasMatch) {
+
+      alert("This email is already registered.");
+      setError("This email is already registered.");
+      
+    } 
+    else{
+      let item={Name_Surname,Address,Email,Password,tel_no}
+      console.warn(item)
+
+      let result = await fetch("http://localhost:2000/api/company",{
+        method:'POST',
+        body: JSON.stringify(item),
+        headers:{
+          "Content-Type":'application/json',
+          "Accept":"application/json"
+        }
+      })
+      result = await result.json();
+      alert("Succesful!")
+      console.warn("Result: ", result)
+
+  }
+}
+
+  return(
+    <>
+      <Head>
+        <title />
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css" />
+        <link rel="stylesheet" href="css/animate.css" />
+        <link rel="stylesheet" href="css/owl.carousel.min.css" />
+        <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+        <link rel="stylesheet" href="css/magnific-popup.css" />
+        <link rel="stylesheet" href="css/aos.css" />
+        <link rel="stylesheet" href="css/ionicons.min.css" />
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css" />
+        <link rel="stylesheet" href="css/jquery.timepicker.css" />
+        <link rel="stylesheet" href="css/flaticon.css" />
+        <link rel="stylesheet" href="css/icomoon.css" />
+        <link rel="stylesheet" href="css/style.css" />
+      </Head>
+      <section className="ftco-section">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6 text-center mb-5">
+              <h2 className="heading-section">Sign Up</h2>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-md-7 col-lg-5">
+              <div className="wrap">
+                <div
+                  className="img"
+                  style={{ backgroundImage: "url(images/bg-1.jpg)" }}
+                />
+                <div className="login-wrap p-4 p-md-5">
+                  <form action="#" className="signin-form">
+
+                    <div className="form-group mt-3">
+                      <label
+                        className="form-control-placeholder"
+                        htmlFor="Email"
+                      >
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        value={Email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="form-control"
+                        name="email"
+                        required=""
+                      />
+                    </div>
+                    <div className="form-group mt-3">
+                      <label
+                        className="form-control-placeholder"
+                        htmlFor="Name_Surname"
+                      >
+                        TC No
+                      </label>
+                      <input
+                        type="text"
+                        value={Name_Surname}
+                        onChange={(e) => setNameSurname(e.target.value)}
+                        className="form-control"
+                        name="name_surname"
+                        required=""
+                      />
+                    </div>
+
+                    {error && <p>{error}</p>}
+                    <div className="form-group">
+                      <button
+                        type="button"
+                        onClick={Register}
+                        className="form-control btn btn-primary rounded submit px-3"
+                      >
+                        Sign In
+                      </button>
+                    </div>
+                  </form>
+                  <p className="text-center">
+                    Already a member?{" "}
+                    <a data-toggle="tab" href="/login">
+                      Sign In
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
-  }
+}
 
-  export default Company;
+
+export default Company;
